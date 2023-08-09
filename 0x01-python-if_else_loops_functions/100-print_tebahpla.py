496a9c3e44bc6ddd2e2d2fd52440b86894ccb359
asoud80 <amgad_a.soud@outlook.com>
2023-08-08 18:15:16 -0700

    100-print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3 """
-for i in range(122, 96, -1):
-    print("{:s}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
+#!/usr/bin/python3
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
