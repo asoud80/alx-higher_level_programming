de1268f7262f362eb1b6cc961000848f1ddb3f59
asoud80 <amgad_a.soud@outlook.com>
2023-08-14 11:19:14 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_x =(), tuple_y =()):
+
+    if len(tuple_x) < 2:
+        if len(tuple_x) == 0:
+            tuple_x = 0, 0
+        else:
+            tuple_x = tuple_x[0], 0
+    if len(tuple_y) < 2:
+        if len(tuple_y) == 0:
+            tuple_y = 0, 0
+        else:
+            tuple_y = tuple_y[0], 0
+
+    return (tuple_x[0] + tuple_y[0], tuple_x[1] + tuple_y[1])
