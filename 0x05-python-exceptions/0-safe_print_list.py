a70a0cf9fa4e02a25284aee09a2661e9e4ef2925
asoud80 <amgad_a.soud@outlook.com>
2023-08-28 11:16:56 -0700

    0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    printed = 0
+    for c in range(0, x):
+        try:
+            print("{}".format(my_list[c]), end="")
+            printed += 1
+        except:
+            continue
+    print()
+    return printed
