342884cddf8e18b58bf8f9e0e8226bab58b3a8f0
asoud80 <amgad_a.soud@outlook.com>
2023-11-14 02:33:33 -0800

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data').dict;
+
 const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(key);
+
+for (const [key, val] of Object.entries(dict)) {
+  newDict[val] ? newDict[val].push(key) : (newDict[val] = [key]);
 }
 console.log(newDict);
