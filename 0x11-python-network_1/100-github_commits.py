dca72c960265908474665073eebe26098e4a7748
asoud80 <amgad_a.soud@outlook.com>
2023-12-01 16:11:56 -0800

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Usage: ./100-github_commits.py <repository name> <repository owner>
-"""
-import sys
+"""Show the lastone ten commits in a repo github"""
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    count = 0
+    page = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[2], argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    req = requests.get(page)
+    for commit in req.json():
+        sha = commit.get("sha")
+        author = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, author))
+        count += 1
+        if count > 9:
+            break
